ccced5bf2b05414cc3d9ed1ffab0841d9414298d
Goodvibes74 <kazibwed499@gmail.com>
2025-05-13 17:05:32 +0300

    Update navigation and form elements: change redirect after registration to 'dashboard', enhance user profile details, and improve form validation

--- sdp_project/core/views.py
+++ sdp_project/core/views.py
@@ -51,7 +51,7 @@ def register(request):
             register_error = "Username already exists."
         else:
             user = User.objects.create_user(username=username, email=email, password=password1)
-            return redirect('home')
+            return redirect('dashboard')
     return render(request, 'core/login_and_register.html', {
         'register_error': register_error,
         'active_tab': 'register',
